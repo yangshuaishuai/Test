6d5a8ab4eb78aca507db426cf692ff693e84bc66
YangShuaiShuai <1214155781@qq.com>
2017-11-06 18:47:28 +0800

    second push code

---
+++ src/com/yss/StringTest.java
@@ -0,0 +1,18 @@
+package com.yss;
+
+public class StringTest {
+
+	public static void main(String[] args) throws Exception {
+
+		String str = "123456";
+		System.out.println(str.substring(1, 3));
+		String cde = "cde";
+		System.out.println("abc" + cde);
+		String c = "abc".substring(2, 3);
+		String d = cde.substring(1, 2);
+		System.out.println(c);
+		short i = 1;
+
+	}
+
+}
---
+++ src/com/yss/ThreadTest.java
@@ -0,0 +1,17 @@
+package com.yss;
+
+public class ThreadTest  extends Thread{
+	
+	public  ThreadTest(String name)
+	{
+		super(name);
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		super.run();
+	}
+	
+	
+
+}
